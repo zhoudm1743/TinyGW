e482b572bbaa687ab4e7e46f714e7710b01974e4
乐清 <empty@inzj.cn>
2025-05-03 10:08:31 +0800

    fix: 为定时任务设置时区为亚洲/上海
    在`ReportTaskServer`和`CollectTaskServer`中，新增定时任务时未指定时区，可能导致任务执行时间与预期不符。通过引入`time.LoadLocation("Asia/Shanghai")`，确保定时任务在正确的时区下执行。

--- pkg/service/task/CollectTaskServer.go
+++ pkg/service/task/CollectTaskServer.go
@@ -4,6 +4,7 @@ import (
 	"github.com/robfig/cron/v3"
 	"go.uber.org/zap"
 	"sync"
+	"time"
 	"tinyGW/app/api/repository"
 	"tinyGW/app/models"
 	"tinyGW/pkg/service/collect"
@@ -62,7 +63,8 @@ func InitCollectTaskServer(collectTaskServer *CollectTaskServer, repository repo
 // Add 新增定时任务，如果定时任务是开启状态，则开启定时任务
 func (cts *CollectTaskServer) Add(task *models.CollectTask) {
 	zap.S().Info("新增定时任务", task)
-	c := cron.New()
+	location, _ := time.LoadLocation("Asia/Shanghai")
+	c := cron.New(cron.WithLocation(location))
 	currentDevices := task.DeviceList
 	c.AddFunc(task.Cron, func() {
 		zap.S().Info("定时任务执行", task)
